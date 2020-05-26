7fc24f3afa1c1cf6c0d0c01878fcae615da8773e
Kurtis <k.soroka@gmail.com>
2020-05-26 09:05:59 -0500

    functional - need to fix save/load state

--- src/store/modules/saveState.js
+++ src/store/modules/saveState.js
@@ -2,36 +2,49 @@
 const namespaced = true;
 
 const state = {
-    savedData: []
+  savedData: []
 }
 
 const mutations = {
-    saveStockGame(state) {
+  saveStockGame(state, payload) {
+    console.log(payload)
+    let tempState = {
+      ownedStocks: payload.ownedStocks,
+      availableStocks: payload.availableStocks,
+      fundsAvailable: payload.fundsAvailable,
+      currentDay: payload.currentDay
+    }
 
-        // state.savedData.ownedStocks = [];
-        // state.savedData.ownedStocks = JSON.parse(JSON.stringify(state.ownedStocks));
-      },
-      loadStockGame(state) {
-        state.ownedStocks = [];
-        state.ownedStocks = JSON.parse(JSON.stringify(state.savedOwnedStocks));
-      }
+    state.savedData = JSON.parse(JSON.stringify(tempState));
+
+  },
+  loadStockGame(state) {
+    let tempState = JSON.parse(JSON.stringify(state));
+    this.state.DayManagement.currentday = tempState.savedData.currentDay;
+    this.state.UserPortfolio.ownedStocks = tempState.savedData.ownedStocks;
+    this.state.AvailableStocks.availableStocks = tempState.savedData.availableStocks;
+    this.state.UserPortfolio.fundsAvailable = tempState.savedData.fundsAvailable;
+  }
 }
 
 const actions = {
-    saveStockGame(context) {
-        context.commit('saveStockGame', {
-          ownedStocks: context.rootGetters['UserPortfolio/listOwnedStocks'],
-          availableStocks: context.rootGetters['AvailableStocks/listAvailableStocks']
-        });
-      },
-      loadStockGame(context) {
-        context.commit('loadStockGame');
-      }
+  saveStockGame(context) {
+    console.dir(context);
+    context.commit('saveStockGame', {
+      ownedStocks: context.rootGetters['UserPortfolio/listOwnedStocks'],
+      availableStocks: context.rootGetters['AvailableStocks/listAvailableStocks'],
+      fundsAvailable: context.rootState.UserPortfolio.availableFunds,
+      currentDay: context.rootGetters['DayManagement/getCurrentDay']
+    });
+  },
+  loadStockGame(context) {
+    context.commit('loadStockGame');
+  }
 }
 
 export default {
-    namespaced,
-    state,
-    actions,
-    mutations
+  namespaced,
+  state,
+  actions,
+  mutations
 }
